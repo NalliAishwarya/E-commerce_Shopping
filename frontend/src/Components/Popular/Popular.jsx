91b0ba5be085793facb2e302ba23d6d847f2f2a5
NalliAishwarya <111627934+NalliAishwarya@users.noreply.github.com>
2024-08-06 15:57:02 +0530

    Update Popular.jsx

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -5,7 +5,7 @@ import Item from '../Item/Item'
 const Popular = () => {
   const [popularProducts,setpopularProducts]=useState([])
   useEffect(()=>{
-    fetch('http://localhost:4000/popularinwomen')
+    fetch('https://e-commerce-shopping-backend.onrender.com/popularinwomen')
     .then((response)=>response.json())
     .then((data)=>setpopularProducts(data));
   },[])
